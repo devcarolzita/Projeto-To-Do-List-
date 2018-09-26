0651c6c0500fab82747a8978cbba7e8ae43b3d84
caabeatriz <contatocarolinebs@gmail.com>
2018-09-26 10:08:43 -0300

    Adicionado botao para check e validação de campos

--- script/script.js
+++ script/script.js
@@ -11,23 +11,54 @@ const button = document.getElementById("inputBtn")
 button.addEventListener("click", function (event) {
     event.preventDefault()
 
+
+    //Validar o campo se estiver nulo ou as outras variaveis
+    if (input.value === "" || input.value === " " || input.value === null || input.value === undefined) {
+        input.focus()
+        return false
+    }
+
+
+    const btnDeleteTask = document.createElement("button")
     const taskDiv = document.createElement("div")
     const task = document.createElement("p")
-    const hr = document.createElement ("hr")
+    const hr = document.createElement("hr")
+
+    //Puxando classe do css para Js
+    btnDeleteTask.className="btnDeleteTask"
+    //Botao excluir tarefas 
+    btnDeleteTask.addEventListener("click", function(){
+        taskDiv.remove()
+    })
 
     task.innerHTML = taskInput.value
+    task.appendChild(btnDeleteTask)
     task.appendChild(hr)
+
     taskDiv.appendChild(task)
     taskBox.appendChild(taskDiv)
 
+    //Após tudo ele deixa o campo em branco    
     taskInput.value = ""
+
+    //Ao clicar nas tarefas ela fica cinza 
+    taskDiv.addEventListener("click", function () {
+        taskDiv.className = "class-styleJss__check"
+        taskDiv.value = ""
+    })
+
+
     
+    
+
 })
 
 
 
-// deleteButton.addEventListener("click" , function(E){
-// E.preventDefault()
-//     taskDiv.remove()
-
-//  })
+//FASE 3 - EXCLUIR TUDO DA LISTA 
+    // deleteButton.addEventListener("click" , function(E){
+    // E.preventDefault()
+    //     taskDiv.remove()
+    
+    //  })
+    
