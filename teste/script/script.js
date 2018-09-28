46dcb190f2a958bfc95719447b4693868ae5ce19
natashamorais <natasha.sousa.morais@gmail.com>
2018-09-28 11:31:42 -0300

    teste drag n' drop funcionando comentado

--- teste/script/script.js
+++ teste/script/script.js
@@ -8,7 +8,13 @@ const deleteButton = document.getElementById("deleteButton")
 const button = document.getElementById("inputBtn")
 const divstylebox = document.querySelector("divstylebox")
 
-form.addEventListener("onsubmit", function (eve) {
+// =---------------------- drag and drop ------------------------------------------
+let dragging  //variavel para guardar a classe a ser movida 
+// O conceito do drag n' drop é pegar um elemento pela classe ou id guardar ela em uma variável e depois colocar esse elemento em outro lugar chamando essa variável
+//--------------------------drag and drop ------------------------------------------
+
+
+form.addEventListener("submit", function (eve) {
     eve.preventDefault()
 })
 
@@ -26,18 +32,15 @@ button.addEventListener("click", function (event) {
     }
 
 
-
-
     const taskDiv = document.createElement("div")
     taskDiv.className = "taskdivclasse"
+    taskDiv.setAttribute('draggable', true)
     const task = document.createElement("p")
-
 
     //Criado div para colocar botao de excluir, dps deu uma classe e por ultimo criou no html
     const btnDeleteTask = document.createElement("div")
     btnDeleteTask.className = "btnDeleteTask"
     btnDeleteTask.innerHTML = ` x `
-
 
     //Botao excluir tarefas 
     btnDeleteTask.addEventListener("click", function (eventdelete) {
@@ -60,36 +63,31 @@ button.addEventListener("click", function (event) {
         // taskDiv.value = ""
     })
     
-    // drag and drop 
-    let dragging  //variavel para guardar a classe 
-
-
-    taskDiv.addEventListener("dragover", function (ev) { //funçãoo permite que objeto seja "droppado"
-
-        
-        function allowDrop(ev) { //permite colocar o objeto arrastado aqui 
-            ev.preventDefault()
-        }
-    })
-
+    // ----- dragstart é onde começa a nossa viagem pelo drag n' drop  ele define que o elemento pode ser arrastado 
     taskDiv.addEventListener("dragstart", function (ev) { // definindo o começo do "arrastão" 
-        id = ev.target.taskdivclasse //define o que vai na variavel 
+        dragging = ev.target.closest('.taskdivclasse'); //aqui definimos o que vamos arrastar no caso a nossa taskdiv - usamos o closest porque ele vai pegar o elemento que tiver a classe ."taskdivclasse" mais próximo do que está selecionado (definido pelo target que pega sempre o elemento o que fez a nossa função funcionar ou seja a taskDiv que a gente clicou) assim não vamos selecionar todas as divs de uma vez 
+        //draging.classList.add('') se vc quiser fazer perfumaria pode fazer assim 
 
     })
-
-    taksDiv.addEventListener("drop", function (ev) { //
-        ev.target.append(document.getElementById(class)) //coloca a variável com o valor a ser "draggeado"(arrastado) no lugar novo 
-
-        //--------------TENHO DÚVIDAS AINDA NESSE CÓDIGO NÃO LEVA TUDO COMO VERDADE ABSOLUTA NÃO, PRINCIPALMENTE NA CRIAÇÃO DE VARIÁVEL 
-        //LINK PRA UM PASSO PASSO https://www.w3schools.com/html/tryit.asp?filename=tryhtml5_draganddrop2
+    //----------dragover é o meio da nossa viagem enquanto estamos arrastando pela tela
+    taskDiv.addEventListener("dragover", function (ev) { //
+        //permite colocar o objeto arrastado aqui 
+        ev.preventDefault();
+        const node = ev.target.closest('.taskdivclasse') // criamos uma nova variável com os mesmo valores da dragging porque queremos a taskdiv que estamos passando por cima então queremos a mesma coisa que antes mas não como uma função que pega o que foi selecionado
+        this.parentNode.insertBefore(dragging, node); // aqui usamos o insertBefore para não ficar por último aqui ele funciona como um appendchild. chamamos o parent node do "this" ou seja do taskdiv que é o taskbox pq é lá que vamos colocar mexer nas div né > definimos que queremos que ele o dragging ou seja a div que selecionamos fique antes do node a div que queremos substituir
     })
+
+///-----------dragend é o final da nossa viagem de drag n' drop
+    taskDiv.addEventListener("dragend", function (ev) { //aqui definimos acabou o nosso arrastão que vai soltar e ficar parada lá 
+        dragging = null;      //usamos o dragging = null para dizer que não é mais para arrastar a não se que vc selecione de novo desencadeando o evento dragstart  
+       
 
     // FASE 3 - MARCAR TODOS OS ITENS COMO FEITO
     const checkButton = document.getElementById("checkButton")
     checkButton.addEventListener("click", function (eventcheck) {
         eventcheck.preventDefault()
         task.className = "class-styleJss__check"
-        // taskDiv.value = ""
+     
     })
 
     //FASE 3 - EXCLUIR TUDO DA LISTA 
@@ -98,10 +96,6 @@ button.addEventListener("click", function (event) {
         taskDiv.remove()
 
     })
-
-
-
-
 })
 
 // var id;
